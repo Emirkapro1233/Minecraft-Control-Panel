a2888b1f5cd42406497852d38478f687af0a5270
Emirkapro1233 <emmmmirkatop@vk.com>
2026-07-16 13:42:09 +0400

    Add Minecraft Control Panel javascript

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.querySelector('.start').onclick = () => {
+ alert('Сервер запускается...');
+};
+
+document.querySelector('.stop').onclick = () => {
+ alert('Сервер остановлен');
+};
+
+document.querySelector('.restart').onclick = () => {
+ alert('Сервер перезапускается...');
+};
